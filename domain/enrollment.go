69d967ac822bd976921e2b21212066fa179485dd
Tomás Zincker <tomas@zincker.io>
2026-03-25 22:48:32 -0300

    corrected model

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -10,13 +10,22 @@ import (
 type Enrollment struct {
 	ID        string     `json:"id" gorm:"type:char(36); not null;primary_key;unique_index"`
 	UserID    string     `json:"user_id,omitempty" gorm:"type:char(36)"`
-	User      *User      `json:"user,omitempty"`
+	User      *User      `json:"user,omitempty" gorm:"-"`
 	CourseID  string     `json:"course_id,omitempty" gorm:"type:char(36)"`
-	Course    *Course    `json:"course,omitempty"`
+	Course    *Course    `json:"course,omitempty" gorm:"-"`
 	Status    string     `json:"status" gorm:"type:char(2)"`
 	CreatedAt *time.Time `json:"-"`
 	UpdatedAt *time.Time `json:"-"`
 }
+
+type EnrollStatus string
+
+const (
+	Pending  EnrollStatus = "P"
+	Active   EnrollStatus = "A"
+	Studying EnrollStatus = "S"
+	Inactive EnrollStatus = "I"
+)
 
 func (e *Enrollment) BeforeCreate(tx *gorm.DB) (err error) {
 	if e.ID == "" {
